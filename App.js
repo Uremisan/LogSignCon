c36dff5dfe31a9599547f5c4799ff0ff1e43eca5
Marley <manfrededun@gmail.com>
2021-08-14 17:10:04 +0000

    first initialization

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView } from 'react-native';
+import LoginForm from './src/screens/LoginForm';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView>
+      <LoginForm />
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
---
+++ src/screens/LoginForm.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { ScrollView, StyleSheet,Text, TextInput, TouchableOpacity } from 'react-native'
+
+export default function LoginForm() {
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.text}>Log In</Text>
+
+      <TextInput
+        placeholder = 'Username' style={styles.input}
+      />
+
+      <TextInput
+        placeholder = 'Password' style={styles.input}
+        secureTextEntry={true}
+        autoCorrect={false}
+      />
+
+      <Text style={styles.input1}>Forgot password?</Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Log In</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.input2}>Don't have account?
+        <TouchableOpacity style={styles.button2}>
+          <Text style={styles.button3}>Sign Up</Text>
+        </TouchableOpacity>
+      </Text>
+      
+    </ScrollView>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 100,
+    marginHorizontal:50,
+  },
+  text: {
+    fontSize: 50,
+    color: '#711be4',
+  },
+  input: {
+    marginTop: 60,
+    fontSize: 23,
+    borderBottomWidth: 3,
+    borderBottomColor: '#711be4',
+  },
+  input1: {
+    paddingTop:10,
+    alignSelf:'flex-end',
+    color: '#7EC8E3',
+  },
+  button: {
+    width:275,
+    height: 55,
+    backgroundColor: '#711be4',
+    borderRadius: 18,
+    marginTop: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    textTransform: "uppercase",
+  },
+
+  input2: {
+    marginTop: 50,
+    alignSelf: 'center',
+    fontSize: 18,
+  },
+  button3: {
+    marginLeft: 10,
+    marginTop: 2,
+    color: '#711be4',
+    
+  },
+
+});
